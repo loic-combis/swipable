648521b710997e9a1391bd10ab9e9e3c30520354
Loïc Combis <loic.combis@rakuten.com>
2021-07-22 11:18:40 +0100

    Support Null Safety
    Fixes #1

--- lib/flutter_swipable.dart
+++ lib/flutter_swipable.dart
@@ -10,66 +10,66 @@ class Swipable extends StatefulWidget {
   /// @param child [Widget]
   /// @required
   /// Swipable content.
-  final Widget child;
+  final Widget? child;
   
   /// Callback
   /// Hook triggered when the card starts being dragged.
   /// @param details [DragStartDetails]
-  final void Function(DragStartDetails details) onSwipeStart;
+  final void Function(DragStartDetails details)? onSwipeStart;
 
   /// Callback
   /// Hook triggered when the card position changes.
   /// @param details [DragUpdateDetails]
-  final void Function(DragUpdateDetails details) onPositionChanged;
+  final void Function(DragUpdateDetails details)? onPositionChanged;
 
   /// Callback
   /// Hook triggered when the card stopped being dragged and doesn't meet the requirement to be swiped.
   /// @param details [DragEndDetails]
-  final void Function(Offset position, DragEndDetails details) onSwipeCancel;
+  final void Function(Offset position, DragEndDetails details)? onSwipeCancel;
 
   /// Callback
   /// Hook triggered when the card stopped being dragged and meets the requirement to be swiped.
   /// @param details [DragEndDetails]
-  final void Function(Offset position, DragEndDetails details) onSwipeEnd;
+  final void Function(Offset position, DragEndDetails details)? onSwipeEnd;
 
   /// Callback
   /// Hook triggered when the card finished swiping right.
   /// @param finalPosition [Offset]
-  final void Function(Offset finalPosition) onSwipeRight;
+  final void Function(Offset finalPosition)? onSwipeRight;
 
   /// Callback
   /// Hook triggered when the card finished swiping left.
   /// @param finalPosition [Offset]
-  final void Function(Offset finalPosition) onSwipeLeft;
+  final void Function(Offset finalPosition)? onSwipeLeft;
 
   /// Callback
   /// Hook triggered when the card finished swiping up.
   /// @param finalPosition [Offset]
-  final void Function(Offset finalPosition) onSwipeUp;
+  final void Function(Offset finalPosition)? onSwipeUp;
 
   /// Callback
   /// Hook triggered when the card finished swiping down.
   /// @param finalPosition [Offset]
-  final void Function(Offset finalPosition) onSwipeDown;
+  final void Function(Offset finalPosition)? onSwipeDown;
 
   /// @param swipe [Stream<double>]
   /// Triggers an automatic swipe.
   /// Cancels automatically after first emission.R
   /// The double value sent corresponds to the direction the card should follow (clockwise radian angle).
-  final Stream<double> swipe;
+  final Stream<double>? swipe;
 
   /// @param animationDuration [int]
   /// Animation duration (in milliseconds) for the card to swipe atuomatically or get back to its original position on swipe cancel.
-  final int animationDuration;
+  final int? animationDuration;
   
   /// @param animationCurve [Curve]
   /// Animation timing function.
-  final Curve animationCurve;
+  final Curve? animationCurve;
 
   /// @param threshold [double]
   /// Defines the strength needed for a card to be swiped.
   /// The bigger, the easier it is to swipe.
-  final double threshold;
+  final double? threshold;
 
   Swipable({
     @required this.child, 
@@ -97,17 +97,15 @@ class SwipableState extends State<Swipable> {
 
   int _duration = 0;
 
-  StreamSubscription _swipeSub;
+  StreamSubscription? _swipeSub;
 
   @override void initState() {
     super.initState();
 
-    if (widget.swipe != null) {
-      _swipeSub = widget.swipe.listen((angle) {
-        _swipeSub.cancel();
-        _animate(angle);
-      });
-    }
+    _swipeSub = widget.swipe?.listen((angle) {
+      _swipeSub?.cancel();
+      _animate(angle);
+    });
   }
 
   @override
@@ -141,10 +139,8 @@ class SwipableState extends State<Swipable> {
   @override
   void dispose() {
     super.dispose();
-
-    if (_swipeSub != null) {
-      _swipeSub.cancel();
-    }
+    
+    _swipeSub?.cancel();
   }
 
   void _onPanUpdate(DragUpdateDetails details) {  
@@ -153,9 +149,7 @@ class SwipableState extends State<Swipable> {
      _positionY += details.delta.dy;
     });
     
-    if (widget.onPositionChanged != null) {
-      widget.onPositionChanged(details);
-    }
+    widget.onPositionChanged!(details);
   }
 
   void _onPanStart(DragStartDetails details) {
@@ -163,9 +157,7 @@ class SwipableState extends State<Swipable> {
       _duration = 0;
     });
 
-    if (widget.onSwipeStart != null) {
-      widget.onSwipeStart(details);
-    }
+    widget.onSwipeStart!(details);
   }
 
   void _onPanEnd(DragEndDetails details) {
@@ -176,8 +168,8 @@ class SwipableState extends State<Swipable> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
 
-    double potentialX = _positionX + (details.velocity.pixelsPerSecond.dx * widget.threshold);
-    double potentialY = _positionY + (details.velocity.pixelsPerSecond.dy * widget.threshold);
+    double potentialX = _positionX + (details.velocity.pixelsPerSecond.dx * (widget.threshold ?? 1));
+    double potentialY = _positionY + (details.velocity.pixelsPerSecond.dy * (widget.threshold ?? 1));
 
     Offset currentPosition = Offset(_positionX, _positionY);
     
@@ -188,49 +180,46 @@ class SwipableState extends State<Swipable> {
       newX = potentialX;
       newY = potentialY;
 
-      if (widget.onSwipeEnd != null) {
-        widget.onSwipeEnd(currentPosition, details);
-      }
+      widget.onSwipeEnd!(currentPosition, details);
+    
     } else {
       newX = 0;
       newY = 0;
 
-      if (widget.onSwipeCancel != null) {
-        widget.onSwipeCancel(currentPosition, details);
-      }
+      widget.onSwipeCancel!(currentPosition, details);
     }
   
     setState(() {
       _positionX =  newX;
       _positionY = newY;
-      _duration = widget.animationDuration;
+      _duration = widget.animationDuration ?? 300;
     });
 
     if (shouldSwipe) {
-      Future.delayed(Duration(milliseconds: widget.animationDuration), () {
+      Future.delayed(Duration(milliseconds: widget.animationDuration ?? 300), () {
         // Clock wise radian angle of the velocity
         double angle = details.velocity.pixelsPerSecond.direction;
         Offset finalPosition = Offset(newX, newY);
         if (angle.abs() <= (math.pi / 4) && widget.onSwipeRight != null) {
-          widget.onSwipeRight(finalPosition);
+          widget.onSwipeRight!(finalPosition);
         } else if (angle.abs() > (3 * math.pi / 4) && widget.onSwipeLeft != null) {
-          widget.onSwipeLeft(finalPosition);
+          widget.onSwipeLeft!(finalPosition);
         } else if (angle >= 0 && widget.onSwipeDown != null) {
-          widget.onSwipeDown(finalPosition);
+          widget.onSwipeDown!(finalPosition);
         } else if (widget.onSwipeUp != null) {
-          widget.onSwipeUp(finalPosition);
+          widget.onSwipeUp!(finalPosition);
         }
     });
     }
   }
 
   void _animate(double angle) {
-    if (angle == null || angle < -math.pi || angle > math.pi) {
+    if (angle < -math.pi || angle > math.pi) {
       throw('Angle must be between -π and π (inclusive).');
     }
     
     if (widget.onSwipeStart != null) {
-      widget.onSwipeStart(DragStartDetails());
+      widget.onSwipeStart!(DragStartDetails());
     }
 
     double width = MediaQuery.of(context).size.width;
@@ -262,8 +251,8 @@ class SwipableState extends State<Swipable> {
     // Calculating velocity so the card arrives at it's final position when the animation ends.
     Velocity velocity = Velocity(
       pixelsPerSecond: Offset(
-        remainingX / widget.threshold, 
-        remainingY / widget.threshold
+        remainingX / (widget.threshold ?? 1), 
+        remainingY / (widget.threshold ?? 1)
       )
     );
     DragEndDetails details = DragEndDetails(velocity: velocity);
